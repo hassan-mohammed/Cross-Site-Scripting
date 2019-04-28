c942817c7b1cd60bc04c9357bed682c453d10ec6
Hassan Mohammed <hassan.aer@gmail.com>
2019-04-28 12:00:35 +0300

    Contextual Output Encoding  & Disable Whitelisting filter

--- Cross-Site-Scripting/greeting.aspx.cs
+++ Cross-Site-Scripting/greeting.aspx.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security.AntiXss;
 
 namespace Cross_Site_Scripting
 {
@@ -25,12 +26,18 @@ namespace Cross_Site_Scripting
             var username = Request.QueryString["q"];
             if (!string.IsNullOrEmpty(username))
             {
+                /////////////Contextual output Encoding////////////////
+                //HTML encoding 
+                lbl_welcome.Text = AntiXssEncoder.HtmlEncode(username, true);
+                //HTML Attribute Encoding 
+                lbl_welcome.Attributes["style"] = "color:red" + AntiXssEncoder.XmlAttributeEncode(username);
+
                 //Whitelisting filter 
-                Regex r = new Regex("^[a-zA-Z0-9 ]*$");
-                if (r.IsMatch(username))
-                    lbl_welcome.Text = "Welcome to our site " + username; 
-                else
-                    lbl_welcome.Text = "Please use characters and numbers only";
+                //Regex r = new Regex("^[a-zA-Z0-9 ]*$");
+                //if (r.IsMatch(username))
+                //    lbl_welcome.Text = "Welcome to our site " + username; 
+                //else
+                //    lbl_welcome.Text = "Please use characters and numbers only";
             }
             else
                 lbl_welcome.Text = "Please let us know who are you!";
